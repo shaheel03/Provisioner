cc871b2abbce09a1709433a93298fa14b7cfb92f
shaheel03 <shaheelkumar@gmail.com>
2024-06-13 13:45:42 +0530

    added code for VM provisioner

---
+++ main.tf
@@ -0,0 +1,93 @@
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "nginix-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "nginix-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "nginix-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = "nginix-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku = "basic"
+}
+
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nginix-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "webapp-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "Jaggy@123456"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  # admin_ssh_key {
+  #   username   = "adminuser"
+  #   public_key = file("~/.ssh/id_rsa.pub")
+  # }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+      "sudo apt-get update",
+      "sudo apt-get install -y npm",
+      "sudo apt-get install -y nginx",
+      "sudo apt-get install -y nodejs",
+      
+    ]
+  }
+    connection {
+      type        = "ssh"
+      user        = "adminuser"
+      password    = "jaggy@123456"
+      host        = azurerm_public_ip.pip.ip_address
+      timeout     = "10m"
+    }
+  }
+
+
